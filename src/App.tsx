24bfaccc165cada32d2317fcfdead0a99fbe2d5e
Peter Francois <peter.francois237@hotmail.com>
2025-08-27 16:28:54 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl text-blue-700 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
